1e6948104ad89b0905d89e5297f2938ea3732dc3
lalaa23 <limcalemon4@gmail.com>
2026-04-14 21:53:24 +0530

    Revert "a"

--- p2.py
+++
@@ -1,9 +0,0 @@
-# program2.py
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-if a > b:
-    print("Greatest number is:", a)
-else:
-    print("Greatest number is:", b)
